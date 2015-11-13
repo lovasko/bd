8bcee51d50a8cd50737850c322a14a5262e60879
Daniel Lovasko <lovasko@freebsd.org>
2015-11-13 02:49:39 +0100

    count option; mandratory newline

--- main.c
+++ main.c
@@ -14,11 +14,12 @@
 
 static uintmax_t group_size;
 static uintmax_t groups_per_line;
-static uintmax_t skip;
+static uintmax_t offset;
+static uintmax_t count;
 static uintmax_t counter;
 static int numbering;
 
-static void
+static int
 print_bytes(char* buffer, ssize_t n_bytes)
 {
 	ssize_t i;
@@ -26,8 +27,11 @@ print_bytes(char* buffer, ssize_t n_bytes)
 	char mask;
 
 	for (i = 0; i < n_bytes * CHAR_BIT; i++, counter++) {
-		if (counter < skip)
+		if (counter < offset)
 			continue;
+
+		if (counter - offset == count)
+			return 1;
 
 		if (numbering == LSB)
 			mask = (char)(1 << (i % CHAR_BIT));
@@ -37,11 +41,13 @@ print_bytes(char* buffer, ssize_t n_bytes)
 		bit = !!(buffer[i/8] & mask);
 		write(STDOUT_FILENO, bit ? "1" : "0", 1);
 
-		if (((counter+1-skip) % (group_size * groups_per_line)) == 0)
+		if (((counter+1-offset) % (group_size * groups_per_line)) == 0)
 			write(STDOUT_FILENO, "\n", 1);
-		else if ((counter+1-skip) % group_size == 0)
+		else if ((counter+1-offset) % group_size == 0)
 			write(STDOUT_FILENO, " ", 1);
 	}
+
+	return 0;
 }
 
 static uintmax_t 
@@ -62,10 +68,11 @@ read_number(void)
 static void
 print_usage(void)
 {
-	printf("bd [-g N] [-l N] [-s N] [file]\n"
+	printf("bd [-c N] [-g N] [-n N] [-o N] [-m|-l] [file]\n"
+	       "  -c N    number of bits\n"
 	       "  -g N    group size\n"
 	       "  -n N    groups per line\n"
-	       "  -s N    skip bits from the head\n"
+	       "  -o N    offset from the head\n"
 	       "  -l      least significant bit first\n"
 	       "  -m      most significant bit first\n");
 }
@@ -80,14 +87,19 @@ main(int argc, char** argv)
 	
 	group_size = CHAR_BIT;
 	groups_per_line = 8;
-	counter = 0;
+	offset = 0;
+	count = UINT_MAX;
 	numbering = LSB;
 
-	while ((option = getopt(argc, argv, "g:hlmn:s:")) != -1) {
+	counter = 0;
+
+	while ((option = getopt(argc, argv, "c:g:hlmn:o:")) != -1) {
 		switch(option) {
 			case 'g': group_size = read_number(); break;
 			case 'n': groups_per_line = read_number(); break;
-			case 's': skip = read_number(); break;
+
+			case 'c': count = read_number(); break;
+			case 'o': offset = read_number(); break;
 
 			case 'l': numbering = LSB; break;
 			case 'm': numbering = MSB; break;
@@ -126,8 +138,12 @@ main(int argc, char** argv)
 			return EXIT_FAILURE;
 		}
 
-		print_bytes(buffer, n_bytes);
+		if (print_bytes(buffer, n_bytes) == 1)
+			break;
 	}
+
+	if (((counter+1-offset) % (group_size * groups_per_line)) != 0)
+		write(STDOUT_FILENO, "\n", 1);
 
 	if (fd != STDIN_FILENO)
 		close(fd);
